0339e94e566b93eae575e259787acab97c5fa280
Tae Min Ha <taemin410@gmail.com>
2019-01-19 18:05:54 +0800

    jaebal

--- data/Preprocessor.py
+++ data/Preprocessor.py
@@ -3,24 +3,43 @@ Data Preprocessor
 """
 import pandas as pd
 from util.Distribution import Distribution
-from data.FileManager import FileManager
 from sklearn.model_selection import train_test_split
 
 class Preprocessor:
-    def __init__(self, dataFrame):
+    def __init__(self):
         '''
         Constructor
 
         :param: data file to be converted into Distribution objects
         '''
         self.__distributionTable = {} # Table having distribution objects (key: name of data, value: distribution object).
-        self.__keys = set(dataFrame.columns.values)    # string type keys for the table.
+        self.__colnames = None # string type keys for the table.
         self.__numOfKeys = 0    # number of keys.
-        self.__dataFrame = dataFrame
-        self.__trainDataFrame = None
-        self.__testDataFrame = None
+        self.__loanData = None # data mainly used.
 
+        self.__attributes_train = None
+        self.__labels_train = None
+
+        self.__attributes_test = None
+        self.__labels_test = None
+
+        self.__retrieve_data()
+        # TODO: function call for preprocessing data
+        self.__temp_data_process()
         self.__split_data()
+
+    def __retrieve_data(self):
+        '''
+        Retrieve the data from the csv file and process to store data to datastructures.
+        Update the row size and colum size.
+
+        :param: name of file (str)
+        :return: data from file
+        '''
+        # TODO: file name should be converted to file path
+        data = pd.read_csv(r"C:\Users\user\Desktop\proj\Data_lympics\Deeplearning\loan.csv")
+        self.__colnames= data.columns.values
+        self.__loanData = data
 
     def data_to_distribution(self):
         '''
@@ -30,7 +49,7 @@ class Preprocessor:
         :return: None
         '''
         # TODO: Deal with string values
-        for key in self.__keys:
+        for key in self.__colnames:
             self.__distributionTable[key] = Distribution(self.__dataFrame, key)
 
     def __split_data(self):
@@ -40,25 +59,47 @@ class Preprocessor:
         :param: whole given data frame
         :return: None
         '''
-        self.__trainDataFrame , self.__testDataFrame = train_test_split(self.__dataFrame, test_size = 0.2)
+        # TODO: loan status may not be the label -> change to label accordingly.
+        X = self.__loanData.drop('loan_status', axis = 1)
+        y = self.__loanData['loan_status']
 
-    def get_train_data(self):
-        '''
-        Retrieve the data frame for training
+        self.__attributes_train, self.__attributes_test, self.__labels_train, self.__labels_test = train_test_split(X, y, test_size=0.2)
 
-        :param: None
-        :return: training data frame
+    def get_train_attributes(self):
         '''
-        return self.__trainDataFrame
-
-    def get_test_data(self):
-        '''
-        Retrieve the data frame for testing
+        Return the attributes of the data for training.
 
         :param: None
-        :return: test data frame
+        :return: data attributes
         '''
-        return self.__testDataFrame
+        return self.__attributes_train
+
+    def get_train_labels(self):
+        '''
+        Return the labels of the data for training.
+
+        :param: None
+        :return: categorical labels
+        '''
+        return self.__labels_train
+
+    def get_test_attributes(self):
+        '''
+        Return the attributes of the data for test.
+
+        :param: None
+        :return: data attributes
+        '''
+        return self.__attributes_test
+
+    def get_test_labels(self):
+        '''
+        Return the labels of the data for test.
+
+        :param: None
+        :return: categorical labels
+        '''
+        return self.__labels_test
 
     def get_distribution(self):
         '''
@@ -76,7 +117,7 @@ class Preprocessor:
         :param:None
         :return: set of strings that represent each feature.
         '''
-        return self.__keys
+        return self.__colnames
 
     def get_feature_size(self):
         '''
@@ -86,3 +127,71 @@ class Preprocessor:
         :return: Number of all the features (int)
         '''
         return self.__numOfKeys
+
+    def __temp_data_process(self):
+
+        dfTrain = self.__loanData
+        # TODO: when dealing with real data, columns has to be selected otherwise
+        #erase unrelated columns
+        dfTrain= dfTrain[['member_id', 'loan_amnt', 'funded_amnt',
+               'term', 'int_rate', 'installment', 'sub_grade',
+               'emp_length', 'annual_inc', 'loan_status']]
+
+        # TODO: Feature transformation can be done beforehand or after
+        # when the data is normalized to numerical data, these steps should be omitted.
+        dfTrain['term'].replace(to_replace=' months', value='', regex=True, inplace=True)
+        dfTrain['term']= pd.to_numeric(dfTrain['term'], errors='coerce')
+
+        #print('Transform: sub_grade...')
+        dfTrain['sub_grade'].replace(to_replace='A', value='0', regex=True, inplace=True)
+        dfTrain['sub_grade'].replace(to_replace='B', value='1', regex=True, inplace=True)
+        dfTrain['sub_grade'].replace(to_replace='C', value='2', regex=True, inplace=True)
+        dfTrain['sub_grade'].replace(to_replace='D', value='3', regex=True, inplace=True)
+        dfTrain['sub_grade'].replace(to_replace='E', value='4', regex=True, inplace=True)
+        dfTrain['sub_grade'].replace(to_replace='F', value='5', regex=True, inplace=True)
+        dfTrain['sub_grade'].replace(to_replace='G', value='6', regex=True, inplace=True)
+        dfTrain['sub_grade'] = pd.to_numeric(dfTrain['sub_grade'], errors='coerce')
+
+        #print('Transform: emp_length...')
+        dfTrain['emp_length'].replace('n/a', '0', inplace=True)
+        dfTrain['emp_length'].replace(to_replace='\+ years', value='', regex=True, inplace=True)
+        dfTrain['emp_length'].replace(to_replace=' years', value='', regex=True, inplace=True)
+        dfTrain['emp_length'].replace(to_replace='< 1 year', value='0', regex=True, inplace=True)
+        dfTrain['emp_length'].replace(to_replace=' year', value='', regex=True, inplace=True)
+        dfTrain['emp_length'] = pd.to_numeric(dfTrain['emp_length'], errors='coerce')
+
+        #print('Transform: annual_inc...')
+        dfTrain['annual_inc']= pd.to_numeric(dfTrain['annual_inc'], errors='coerce')
+
+        #print('Transform: loan_status...')
+        # for loan status just gave random 0 / 1 of binary representation of good or bad loan
+        dfTrain['loan_status'].replace('n/a', '0', inplace=True)
+        dfTrain['loan_status'].replace(to_replace='Fully Paid', value='0', regex=True, inplace=True)
+        dfTrain['loan_status'].replace(to_replace='Current', value='1', regex=True, inplace=True)
+        dfTrain['loan_status'].replace(to_replace='Charged Off', value='2', regex=True, inplace=True)
+        dfTrain['loan_status'].replace(to_replace='In Grace Period', value='3', regex=True, inplace=True)
+        dfTrain['loan_status'].replace(to_replace='Late (31-120 days)', value='4', regex=True, inplace=True)
+        dfTrain['loan_status'].replace(to_replace='Late (16-30 days)', value='5', regex=True, inplace=True)
+        dfTrain['loan_status'].replace(to_replace='Issued', value='6', regex=True, inplace=True)
+        dfTrain['loan_status'].replace(to_replace='Default', value='7', regex=True, inplace=True)
+        dfTrain['loan_status'].replace(to_replace='Does not meet the credit policy. Status:Fully Paid Off', value='8', regex=True, inplace=True)
+        dfTrain['loan_status'].replace(to_replace='Does not meet the credit policy. Status:Charged Off', value='9', regex=True, inplace=True)
+        dfTrain['loan_status'] = pd.to_numeric(dfTrain['loan_status'], errors='coerce')
+
+
+        '''
+        #data imputation
+        '''
+        cols = ['term', 'loan_amnt', 'funded_amnt', 'int_rate', 'sub_grade', 'annual_inc', 'emp_length', 'installment']
+        for col in cols:
+            print('Imputation with Median: %s' % (col))
+            dfTrain[col].fillna(dfTrain[col].median(), inplace=True)
+
+        cols=['member_id', 'loan_status']
+        for col in cols:
+            print('Imputation with Zero: %s' % (col))
+            dfTrain[col].fillna(0, inplace=True)
+        print('Missing value imputation done.')
+
+
+        self.__loanData = dfTrain
--- learning/Models.py
+++ learning/Models.py
@@ -2,9 +2,11 @@
 Model object that contains all the possible classification models
 '''
 import pandas as pd
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, confusion_matrix
+from data.Preprocessor import Preprocessor
 
 import xgboost
 from sklearn.metrics import accuracy_score
@@ -13,42 +15,95 @@ from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import train_test_split
 
 class Models:
-    def __init__(self, dataFrame):
+    def __init__(self):
         self.__algorithms = set() # list containing all the algorithms (str)
-        self.__data = dataFrame
+        self.__processor = Preprocessor() # processor managing data
 
 
-    def binary_logistic_regression(self, targetY):
-        #targetY is the column name @param string
+    def binary_logistic_regression(self):
+        """
+
+        """
         # TODO: code by taemin
+        print("Hello I'm binary logistic regression")
 
-        dfTrain, dfTest = train_test_split(self.__data ,test_size=0.2)
+        trainAttributes = self.__processor.get_train_attributes()
+        trainLabels = self.__processor.get_train_labels()
+        testAttributes = self.__processor.get_test_attributes()
+        testLabels = self.__processor.get_test_labels()
 
-        #test_member_id = pd.DataFrame(dfTest['member_id'])
-        train_target = pd.DataFrame(dfTrain[targetY])
+        print(trainAttributes.shape)
+        print(trainLabels.shape)
+        print(testAttributes.shape)
+        print(testLabels.shape)
 
-        X_train, X_test, y_train, y_test = train_test_split(np.array(finalTrain), np.array(train_target), test_size=0.20)
-        eval_set=[(X_test, y_test)]
+        eval_set=[(testAttributes, testLabels)]
 
         clf = xgboost.sklearn.XGBClassifier(
-            objective="binary:logistic",
+            objective="multi:softprob",
             learning_rate=0.05,
             seed=9616, #seed that is not random
             max_depth=20,
             gamma=10,
             n_estimators=500)
 
-        clf.fit(X_train, y_train, early_stopping_rounds=10, eval_metric="auc", eval_set=eval_set, verbose=True)
-        #stops training if 10 rounds of estimation is same
-        """
-        print(datetime.now()-st)
-        #calculate time of training
-        """
+        clf.fit(trainAttributes, trainLabels, early_stopping_rounds=10, eval_metric="auc", eval_set=eval_set, verbose=True)
 
-        y_pred = clf.predict(X_test)
+        y_pred = clf.predict(testAttributes)
 
-        accuracy = accuracy_score(np.array(y_test).flatten(), y_pred)
+        accuracy = accuracy_score(np.array(testLabels).flatten(), y_pred)
         print("Accuracy: %.10f%%" % (accuracy * 100.0))
 
-        accuracy_per_roc_auc = roc_auc_score(np.array(y_test).flatten(), y_pred)
+        accuracy_per_roc_auc = roc_auc_score(np.array(testLabels).flatten(), y_pred)
         print("ROC-AUC: %.10f%%" % (accuracy_per_roc_auc * 100))
+
+        print("Hello I'm binary logistic regression")
+
+
+    def linear_SVM(self):
+        '''
+        Support Vector Machine algorithm for categorical classification.
+        Kernel: linear
+
+        :param: None
+        :return None
+        '''
+        trainAttributes = self.__processor.get_train_attributes()
+        trainLabels = self.__processor.get_train_labels()
+        testAttributes = self.__processor.get_test_attributes()
+        testLabels = self.__processor.get_test_labels()
+
+        #train svm model
+        svclassifier = SVC(kernel = 'linear')
+        svclassifier.fit(trainAttributes, trainLabels)
+
+        #make prediction
+        label_prediction = svclassifier.predict(testAttributes)
+
+        #evaluation
+        print(confusion_matrix(testLabels, label_prediction))
+        print(clasification_report(testLabels, label_prediction))
+
+    def gaussian_SVM(self):
+        '''
+        Support Vector Machine algorithm for categorical classification.
+        Kernel: gaussian
+
+        :param: None
+        :return: None
+        '''
+        trainAttributes = self.__processor.get_train_attributes()
+        trainLabels = self.__processor.get_train_labels()
+        testAttributes = self.__processor.get_test_attributes()
+        testLabels = self.__processor.get_test_labels()
+
+        #train svm model
+        svclassifier = SVC(kernel = 'rbf')
+        svclassifier.fit(trainAttributes, trainLabels)
+
+        #make prediction
+        label_prediction = svclassifier.predict(testAttributes)
+
+        #evaluation
+        print(confusion_matrix(testLabels, label_prediction))
+        print(clasification_report(testLabels, label_prediction))
