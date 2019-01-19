b6920330ff1768161b2900d9623522c0a0e6c262
choijaewon <choijaewon959@gmail.com>
2019-01-19 14:21:27 +0800

    models added 2

--- learning/Models.py
+++ learning/Models.py
@@ -2,8 +2,9 @@
 Model object that contains all the possible classification models
 '''
 class Models:
-    def __init__(self):
-        __algorithms = set() # list containing all the algorithms (str)
+    def __init__(self, dataFrame):
+        self.__algorithms = set() # list containing all the algorithms (str)
+        self.__data = dataFrame
 
     def binary_logistic_regression(self):
-        # TODO: code by taemin 
+        # TODO: code by taemin
